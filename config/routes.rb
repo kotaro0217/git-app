80cb9087548f2581e5c62d75e810e2e2e11780f6
kotaro0217 <72353452+kotaro0217@users.noreply.github.com>
2021-12-03 19:34:28 +0900

    end commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
   root 'posts#index'
 end
-=======
-end
->>>>>>> parent of fe37f5c... Update routes.rb
+
